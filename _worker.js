64f7e9ccd2071d9b1b2a32da4a7c0e33f17f90d4
XDEandTOMs <95138542+XDEandTOMs@users.noreply.github.com>
2022-08-01 14:35:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="www.xdeandtom.xyz";
+        url.hostname="cf.xdeandtom.xyz";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
